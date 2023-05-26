a9236c9c9393d7683d6b85b7e110890cb693cdcc
unknown <eliyahulevinson@gmail.com>
2023-05-25 21:53:33 -0400

    removed public middleware

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(helmet());
 // app.use(cors());
 app.use(xss());
 
-app.use(express.static(path.resolve(__dirname, './client/build')));
+// app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(appLimiter);
 
 // routes
