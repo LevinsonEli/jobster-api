f11ffe8413c88b5a0f71f5a3f3298675296bd729
unknown <eliyahulevinson@gmail.com>
2023-06-18 21:08:54 -0400

    test

--- server/src/api/middlewares/error-handler.ts
+++ server/src/api/middlewares/error-handler.ts
@@ -14,7 +14,6 @@ const errorHandlerMiddleware = (
 ) => {
   console.log(err);
   let customError = {
-    // set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong try again later',
   };
