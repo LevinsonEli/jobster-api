dd35e7620ded74ecbe64f7e545c54ebda95b04c5
unknown <eliyahulevinson@gmail.com>
2023-06-18 21:53:39 -0400

    fixed sec issue

--- server/src/loaders/db/populate.ts
+++ server/src/loaders/db/populate.ts
@@ -3,12 +3,11 @@ require('dotenv').config();
 const mockData = require('../../mock-data.json');
 const Job = require('../models/Job');
 import connectDB from './connect';
+import config from '../../config';
 
 const start = async () => {
   try {
-    await connectDB(
-      'mongodb+srv://mongoDBuser:123petrol321mong@nodeexpressprojects.7kp2d.mongodb.net/jobster-manager-tutorial?retryWrites=true&w=majority'
-    ); // please use config file/folder to handle this
+    await connectDB(config.databaseURL);
     await Job.create(mockData);
     console.log('Successfully populated the DB');
     process.exit(0);
